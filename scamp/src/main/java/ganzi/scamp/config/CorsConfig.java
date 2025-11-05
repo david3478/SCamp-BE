057a3af6f9b50550591b3adb61bd8e149d0dce42
david3478 <inwookim03@naver.com>
2025-11-05 21:23:31 +0900

    cors config

--- scamp/src/main/java/ganzi/scamp/config/CorsConfig.java
+++ scamp/src/main/java/ganzi/scamp/config/CorsConfig.java
@@ -1,20 +1,20 @@
-//package ganzi.scamp.config;
-//
-//import org.springframework.context.annotation.Configuration;
-//import org.springframework.web.servlet.config.annotation.CorsRegistry;
-//import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-//
-//@Configuration
-//public class CorsConfig implements WebMvcConfigurer {
-//
-//
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry.addMapping("/**")
-//                .allowedOrigins("*")
-//                .allowedMethods("*")
-//                .allowedHeaders("*")
-//                .allowCredentials(true)
-//                .maxAge(3600);
-//    }
-//}
+package ganzi.scamp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("https://s-camp.vercel.app/")
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
--- scamp/src/main/java/ganzi/scamp/config/SwaggerConfig.java
+++ scamp/src/main/java/ganzi/scamp/config/SwaggerConfig.java
@@ -1,7 +1,9 @@
 package ganzi.scamp.config;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -16,6 +18,8 @@ public class SwaggerConfig {
                 .description("<h3>Contact Service Swagger</h3>");
 
         return new OpenAPI()
+                .components(new Components())
+                .addServersItem(new Server().url("/")) // 추가
                 .info(info);
     }
 }
