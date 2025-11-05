2f5df1d59f52b0008a5c91c4fd83e99afbddeb0b
david3478 <inwookim03@naver.com>
2025-11-05 18:36:00 +0900

    test

---
+++ scamp/src/main/java/ganzi/scamp/ScampApplication.java
@@ -0,0 +1,13 @@
+package ganzi.scamp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScampApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ScampApplication.class, args);
+    }
+
+}
---
+++ scamp/src/test/java/ganzi/scamp/ScampApplicationTests.java
@@ -0,0 +1,13 @@
+package ganzi.scamp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScampApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
